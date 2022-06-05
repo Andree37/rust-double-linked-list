d9634da67d2d7fe8f562ecbb186b07c1b9fa883a
Andre <33375224+Andree37@users.noreply.github.com>
2022-06-05 18:59:18 +0100

    into iter

--- lists/src/second.rs
+++ lists/src/second.rs
@@ -12,6 +12,8 @@ struct Node<T> {
     next: Link<T>,
 }
 
+pub struct IntoIter<T>(List<T>);
+
 
 impl<T> List<T> {
     pub fn new() -> Self {
@@ -45,6 +47,10 @@ impl<T> List<T> {
             &mut node.elem
         })
     }
+
+    pub fn into_iter(self) -> IntoIter<T> {
+        return IntoIter(self);
+    }
 }
 
 impl<T> Drop for List<T> {
@@ -54,6 +60,15 @@ impl<T> Drop for List<T> {
         while let Some(mut boxed_node) = cur_link {
             cur_link = mem::replace(&mut boxed_node.next, None);
         }
+    }
+}
+
+impl<T> Iterator for IntoIter<T> {
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        // access fields of a tuple struct numerically
+        return self.0.pop();
     }
 }
 
@@ -107,5 +122,19 @@ mod test {
 
         assert_eq!(list.peek(), Some(&42));
         assert_eq!(list.pop(), Some(42));
+    }
+
+    #[test]
+    fn into_iter() {
+        let mut list = List::new();
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        let mut iter = list.into_iter();
+        assert_eq!(iter.next(), Some(3));
+        assert_eq!(iter.next(), Some(2));
+        assert_eq!(iter.next(), Some(1));
+        assert_eq!(iter.next(), None);
     }
 }
